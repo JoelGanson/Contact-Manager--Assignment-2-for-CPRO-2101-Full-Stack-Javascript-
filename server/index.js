58c23b680c034159112757f937e791eb711f5ad3
Flabort <Joel.ganson@rdpolytech.ca>
2024-02-27 15:59:05 -0700

    Create index.js and .env and then connect to MongoDB

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoos");
+const env = require("dotenv");
+env.config();
+const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.dbusername}:${process.env.dbpassword}@contactmanager.o7xaklf.mongodb.net/`
+  )
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const x = "x";
