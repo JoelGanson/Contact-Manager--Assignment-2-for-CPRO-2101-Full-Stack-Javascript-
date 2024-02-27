5690ca6bf4d59095acc93b2a4df85d63de29de78
Flabort <Joel.ganson@rdpolytech.ca>
2024-02-27 16:26:37 -0700

    Add update to controller, add timestamps to model

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -20,7 +20,26 @@ const createContact = async (req, res) => {
   }
 };
 
-const updateContact = async (req, res) => {};
+const updateContact = async (req, res) => {
+    try {
+        let contactId = req.body.contactID;
+        const contact = new Contact({
+          FirstName: req.body.FirstName,
+          LastName: req.body.LastName,
+          Phone: req.body.Phone,
+          Email: req.body.Email,
+          CategoryId: req.body.CategoryId,
+          Organization: req.body.Organization,
+        });
+    
+        const contactData = await Contact.findByIdAndUpdate(contactId, {$set:contact});
+        res
+          .status(200)
+          .send({ success: true, msg: "Contact Data", data: contactData });
+      } catch (error) {
+        res.status(200).send({ success: false, msg: error.message });
+      }
+};
 
 module.exports = {
   createContact,
--- server/models/contactModel.js
+++ server/models/contactModel.js
@@ -1,29 +1,34 @@
 const mongoose = require("mongoose");
-const contactSchema = mongoose.Schema({
-  FirstName: {
-    type: String,
-    required: true,
+const contactSchema = mongoose.Schema(
+  {
+    FirstName: {
+      type: String,
+      required: true,
+    },
+    LastName: {
+      type: String,
+      required: true,
+    },
+    Phone: {
+      type: String,
+      required: true,
+    },
+    Email: {
+      type: String,
+      required: true,
+    },
+    CategoryId: {
+      type: String,
+      required: true,
+    },
+    Organization: {
+      type: String,
+      required: false,
+    },
   },
-  LastName: {
-    type: String,
-    required: true,
-  },
-  Phone: {
-    type: String,
-    required: true,
-  },
-  Email: {
-    type: String,
-    required: true,
-  },
-  CategoryId: {
-    type: String,
-    required: true,
-  },
-  Organization: {
-    type: String,
-    required: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("post", postSchema);
