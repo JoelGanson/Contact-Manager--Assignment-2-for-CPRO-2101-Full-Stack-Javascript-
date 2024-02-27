2eff35a6c89590e076b1475facfd7e03fd31e82a
Flabort <Joel.ganson@rdpolytech.ca>
2024-02-27 16:44:51 -0700

    Create Routes for Create and Update

---
+++ server/routes/contact.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const ContactController = require("../controllers/contactController");
+const Contact = require("../models/contactModel");
+
+router.post("/create", ContactController.createContact);
+router.put("/update", ContactController.updateContact);
