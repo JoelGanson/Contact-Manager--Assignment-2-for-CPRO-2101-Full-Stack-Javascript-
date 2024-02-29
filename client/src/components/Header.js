c463ef8eccbba2e2838b431305c433ed17612488
Flabort <Joel.ganson@rdpolytech.ca>
2024-02-28 23:35:22 -0700

    Animated gif as header background

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,13 @@
 export default function Header() {
   // This is simple, just makes sure that all the pages share the same header
   return (
-    <center className="container pt-5 my-5 bg-dark text-white">
+    <center
+      className="container pt-5 my-5 bg-dark text-white"
+      style={{
+        "background-image": "url('https://i.imgur.com/VE4Ebp1.gif')",
+        "background-size": "calc(100% + 40px)",
+      }}
+    >
       <h1>Contact Manager</h1>
     </center>
   );
